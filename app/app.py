26c4b8019456702de76bf4fe552d3975e4986040
MohammadRzMahdyan <mohammadrzmahdyan@gmail.com>
2026-02-26 15:44:42 +0330

    added embarked field app/app.py

--- app/app.py
+++ app/app.py
@@ -10,32 +10,29 @@ from transformers.custom_transformers import *
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "../outputs/models/titanic_xgb_model.pkl")
 
-# ------------------ Page Config ------------------
 st.set_page_config(
     page_title="Titanic Survival App",
     page_icon="🚢",
     layout="wide"
 )
 
-# ------------------ Load Model ------------------
 @st.cache_resource
 def load_model():
     return joblib.load(MODEL_PATH)
 
 model = load_model()
 
-# ------------------ Sidebar Inputs ------------------
 with st.sidebar:
     st.header("Passenger Info")
     pclass = st.selectbox("Class", [1,2,3])
     gender = st.selectbox("Gender", ["Male","Female"])
+    embarked = st.selectbox("Embarked", ["S","C", 'Q'])
     age = st.slider("Age", 0, 100, 25)
     fare = st.number_input("Fare", 0.0, 500.0, 50.0)
     sibsp = st.number_input("Siblings/Spouses", 0, 10, 0)
     parch = st.number_input("Parents/Children", 0, 10, 0)
     predict_button = st.button("Predict Survival")
 
-# ------------------ Prepare Input for Pipeline ------------------
 def make_input():
     return pd.DataFrame({
         'pclass': [pclass],
@@ -46,10 +43,9 @@ def make_input():
         'ticket': ["unknown"],
         'fare': [fare],
         'age': [age],
-        'embarked': [np.nan] 
+        'embarked': [embarked] 
     })
 
-# ------------------ Main Layout ------------------
 st.header("🚢 Titanic Survival Predictor")
 
 if predict_button:
